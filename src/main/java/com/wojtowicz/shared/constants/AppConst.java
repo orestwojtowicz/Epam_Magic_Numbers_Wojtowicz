39c93c9757efb54b1ba4e51db403c8045109d2e5
orestwojtowicz <damianwojtowicz94@gmail.com>
2020-01-27 13:43:54 +0100

    changed AppConst.class & add support for maven

---
+++ src/main/java/com/wojtowicz/shared/constants/AppConst.java
@@ -0,0 +1,26 @@
+package com.wojtowicz.shared.constants;
+
+/**
+ * @author Damian Wójtowicz
+ * @project Epam_Magic_Number
+ * @date 25.01.20
+ */
+
+public class AppConst {
+
+
+    /**
+     * App constants for holding paths to all files, which can be checked for extensions
+     * */
+
+    private static String ROOT_PATH = "src/main/resources/";
+
+
+    public static final String ANGULAR_PNG = ROOT_PATH + "angular.png";
+    public static final String NOT_GIF = ROOT_PATH + "notgif.gif";
+    public static final String QUESTIONS_PDF = ROOT_PATH + "questions.pdf";
+    public static final String RESUME_PDF = ROOT_PATH + "resume.pdf";
+
+
+
+}
